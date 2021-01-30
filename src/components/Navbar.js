266884199f92fa43d88323c4167254d20030f537
devdave0 <dbchung24@gmail.com>
2021-01-30 18:01:21 -0500

    set links on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ function Navbar() {
 
     const handleClick = () => setClick(!click);
 
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
             <nav className='navbar'>
@@ -16,6 +18,29 @@ function Navbar() {
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                                Projects
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                                About Me
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contact' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Contact Me
+                            </Link>
+                        </li>
+                        
+                    </ul>
                 </div>
             </nav>
         </>
