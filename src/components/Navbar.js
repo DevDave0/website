ad3746c2f03e034aced4e9cf3e150765eceff531
devdave0 <dbchung24@gmail.com>
2021-01-30 17:37:55 -0500

    set handle click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    Navbar container
+                    <Link to='/' className='navbar-logo'>
+                        DC
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
                 </div>
             </nav>
         </>
