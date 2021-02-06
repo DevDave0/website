67020a15ebc4cd5860aaedfdb45c889e0cb3e449
devdave0 <dbchung24@gmail.com>
2021-02-06 09:44:48 -0800

    create card item and cards

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Button } from './Button'
+import './Hero.css';
+import '../App.css';
 
 function Hero() {
     return (
         <div className='hero-container'>
-            <video src='/videos/video-2.mp4' autoPlay loop muted />
             <h1>DAVID CHUNG</h1>
             <p>Full Stack Web Developer</p>
             <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
-                    ABOUT ME
+                    SIGN UP
+                </Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+                    SIGN UP
                 </Button>
             </div>
         </div>
