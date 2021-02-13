d52bd6b9339045e67ac3d6adc77f63460dbeea6f
devdave0 <dbchung24@gmail.com>
2021-02-12 22:32:31 -0500

    fixed images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+import image9 from '../images/img-9.jpg'
 
 function Cards() {
     return (
@@ -9,7 +10,12 @@ function Cards() {
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
-                        <CardItem />
+                        <CardItem 
+                        src={image9}
+                        text='Test test test'
+                        label='test'
+                        path='/services'
+                        />
                     </ul>
                 </div>
             </div>
